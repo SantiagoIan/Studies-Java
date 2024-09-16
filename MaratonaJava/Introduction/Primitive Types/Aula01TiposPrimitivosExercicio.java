f79b7d7bc59fcfc2c7526048a42bbb3df70fa5b8
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-09-16 15:57:45 -0300

    Create Aula01TiposPrimitivosExercicio.java

---
+++ MaratonaJava/Introduction/Primitive Types/Aula01TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package maratonajava.devdojo.introducao.TiposPrimitivos;
+
+public class Aula01TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        double salary = 1420.99;
+        String adress = "Av. Paulista";
+        String name = "Santiago";
+        String date = "02/05/2024";
+
+        String relatory = "Eu " + name + " morando em " + adress + " confirmo que recebi o sálario de " + salary + " na data: " + date;
+
+        System.out.println(relatory);
+    }
+}
