bb537465580a58b33863a35a85bf9e683eb32327
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-09-20 16:22:14 -0300

    Create Aula05Arrays01.java

---
+++ MaratonaJava/Introduction/Arrays/Aula05Arrays01.java
@@ -0,0 +1,14 @@
+package maratonaJava.devDojo.introducao.arrays;
+
+public class Aula05Arrays01 {
+    public static void main(String[] args) {
+        int[]idades = new int[3];
+        idades[0] = 18;
+        idades[1] = 20;
+        idades[2] = 55;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
